843ceb81093f70cbab1e36a9c157f0ee7b6c6877
Raghav-Pal <automtion.devops@gmail.com>
2019-06-12 12:09:08 +0530

    First project commit

---
+++ Test Suites/Day3/TestSuite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f717849e-c3a7-4f67-bf87-7b7d86996b4e</testSuiteGuid>
+   <testCaseLink>
+      <guid>1bd00769-f002-4324-8cb1-db452b9e73b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day3/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
